b933b66775ebdfee4475fb0531a757131f65e2d8
nubelsondev <nubelsondev@hotmail.com>
2020-01-06 00:38:27 +0000

    feat: Building the Header

--- src/sections/header.js
+++ src/sections/header.js
@@ -1,7 +1,24 @@
 import React from "react"
 
+import logo from "../img/logo-white.png"
+
 const Header = () => {
-    return <header className="header"></header>
+    return (
+        <header className="header">
+            <div className="logo-box">
+                <img src={logo} alt="Logo" className="logo" />
+            </div>
+
+            <div className="text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary--main">Outdoors</span>{" "}
+                    <span className="heading-primary--sub">
+                        is where life happens
+                    </span>
+                </h1>
+            </div>
+        </header>
+    )
 }
 
 export default Header
